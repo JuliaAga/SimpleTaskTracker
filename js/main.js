9c0230144ce8d14c478a32c6a47a10716513fc9f
JuliaAga <dongro@yandex.ru>
2020-09-29 11:51:53 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+var app = new Vue({
+    el: '#app2',
+    data: {
+        newTask: "",
+        tasks: ["Первая задача", "Вторая задача", "Третья задача"]
+    },
+    methods: {
+        addTask: function(){
+            this.tasks.push(this.newTask);
+            this.newTask = "";
+        },
+        editTask: function(i){
+            let newTask = prompt("Измените задачу:", this.tasks[i]);
+            Vue.set(this.tasks, i, newTask);
+        },
+        deleteTask: function(i){
+            this.tasks.splice(i, 1);
+        }
+    }
+});
